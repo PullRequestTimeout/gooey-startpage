24fcd7b39013c2d6de0ff230e8763fd506703fde
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-01-11 19:20:09 -0800

    :bug: Wrote tabindex handling function, now have to attach it to all events

--- src/animation.js
+++ src/animation.js
@@ -51,3 +51,39 @@ const hideErrorModal = () => {
 }
 
 document.getElementById("modalOk").addEventListener("click", hideErrorModal);
+
+// TabIndex Accessiblity Fix ----------------------------------------------------------------------
+
+// Remove settings panels from Tab Index until visible
+// Function looks like shit but should be fairly extensible
+
+const dynamicTabIndex = () => {
+    // Creates array from all tab elements in settings panel
+    const settingsPanel = document.getElementById("settingsPanel")
+    const settingsButtons = Array.from(settingsPanel.getElementsByTagName("button"))
+    const settingsInputs = Array.from(settingsPanel.getElementsByTagName("input"))
+    const settingsLinks = Array.from(settingsPanel.getElementsByTagName("a"))
+    const allTabIndexedSettingsElements = settingsButtons.concat(settingsInputs, settingsLinks)
+    
+    // Creates array from all tab elements in edit links panel
+    const editLinksPanel = document.getElementById("editLinksPanel");
+    const editLinksButtons = Array.from(editLinksPanel.getElementsByTagName("button"))
+    const editLinksInputs = Array.from(editLinksPanel.getElementsByTagName("input"))
+    const editLinksLinks = Array.from(editLinksPanel.getElementsByTagName("a"))
+    const allTabIndexedEditLinksElements = editLinksButtons.concat(editLinksInputs, editLinksLinks)
+
+    // If panels aren't displayed currently, removes arrays of focusable elements from the tabindex
+    if (settingsPanel.classList[settingsPanel.classList.length - 1] !== 'display-settings') {
+        allTabIndexedSettingsElements.forEach(element => element.setAttribute("tabindex", "-1"))
+    } else {
+        allTabIndexedSettingsElements.forEach(element => element.removeAttribute("tabindex"))
+    }
+
+    if (editLinksPanel.classList[editLinksPanel.classList.length - 1] !== 'display-settings') {
+        allTabIndexedEditLinksElements.forEach(element => element.setAttribute("tabindex", "-1"))
+    } else {
+        allTabIndexedEditLinksElements.forEach(element => element.removeAttribute("tabindex"))
+    }
+}
+
+document.body.onload = dynamicTabIndex();
