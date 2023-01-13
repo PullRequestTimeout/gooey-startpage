3dfaf8c8a1e764212c429b4466b89dbdedeac913
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-01-12 20:08:18 -0800

    Changed default links for release

--- src/editlinks.js
+++ src/editlinks.js
@@ -31,16 +31,6 @@ const defaultLinkList = [
         </svg>`,
     },
     {
-        linkName: "1337x",
-        linkURL: "https://1337x.to/",
-        linkSVG: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-download" width="100" height="100" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
-        <polyline points="7 11 12 16 17 11" />
-        <line x1="12" y1="4" x2="12" y2="16" />
-        </svg>`,
-    },
-    {
         linkName: "Prime Video",
         linkURL: "https://www.primevideo.com/",
         linkSVG: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-player-play" width="100" height="100" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -50,10 +40,21 @@ const defaultLinkList = [
     },
     {
         linkName: "GitHub",
-        linkURL: "https://github.com/PullRequestTimeout",
+        linkURL: "https://github.com/",
         linkSVG: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="100" height="100" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
         <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+        </svg>`,
+    },
+    {
+        linkName: "Gmail",
+        linkURL: "https://mail.google.com/mail/",
+        linkSVG: `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-gmail" width="100" height="100" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M16 20h3a1 1 0 0 0 1 -1v-14a1 1 0 0 0 -1 -1h-3v16z" />
+        <path d="M5 20h3v-16h-3a1 1 0 0 0 -1 1v14a1 1 0 0 0 1 1z" />
+        <path d="M16 4l-4 4l-4 -4" />
+        <path d="M4 6.5l8 7.5l8 -7.5" />
         </svg>`,
     },
     {
